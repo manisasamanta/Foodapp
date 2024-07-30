08bf8eeb5cd2fb432a2eb85a0f6e8f413bca2d86
SayantanCode <sayantan648@gmail.com>
2024-07-30 22:47:28 +0530

    order done

---
+++ app/controller/user/apiController/orderController.js
@@ -0,0 +1,28 @@
+const Order = require("../../../models/orderModel");
+const Cart = require("../../../models/addToCartModel");
+const mongoose = require("mongoose");
+class OrderController {
+    addOrder = async (req, res) => {
+        try {
+            const order = new Order({
+                user: req.user.id,
+                menu: req.query.menu,
+                restaurant: req.query.restaurant,
+                quantity: req.query.count,
+                orderStatus: "Pending"
+            })
+            await order.save()
+            await Cart.updateOne(
+                { user: (req.user.id) },
+                { $pull: { cart: { menu: new mongoose.Types.ObjectId(req.query.menu) } } }
+              );
+              return res.redirect("/cart")
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: error.message
+            })
+        }
+    }
+}
+module.exports = new OrderController()
