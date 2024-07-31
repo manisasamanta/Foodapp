eb5affac0367e84e3443577da28508a4c6fc514b
SayantanCode <sayantan648@gmail.com>
2024-07-31 10:01:02 +0530

    some file got deleted due to conflit, I fixed some part

--- app/routes/user/apiRoutes.js
+++ app/routes/user/apiRoutes.js
@@ -4,6 +4,7 @@ const userCartController = require("../../controller/user/apiController/userCart
 const { verifyToken } = require("../../middleware/auth");
 const searchController = require("../../controller/user/searchController");
 const commentController = require("../../controller/user/apiController/commentsController");
+const orderController = require("../../controller/user/apiController/orderController");
 const router = express.Router();
 router.post("/signup", userAuthController.signUp);
 router.post("/login", userAuthController.login);
@@ -14,5 +15,6 @@ router.get("/logout", verifyToken, userAuthController.logout);
 router.get("/cart/add/:_id", verifyToken, userCartController.addCart)
 router.get("/cart/remove/:_id", verifyToken, userCartController.removeCart)
 router.post("/blog/comment/add", verifyToken, commentController.addComment)
-router.get("/blog/search", searchController.blogSearch)
+router.get("/blog/search", verifyToken, searchController.blogSearch)
+router.get("/order", verifyToken, orderController.addOrder)
 module.exports = router
